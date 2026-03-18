ed344f0e5237c3aea6460c212abfdcc4fecd0887
sebastianJRG <seba.iron28@gmail.com>
2026-03-17 22:09:07 -0300

    init commit

---
+++ prisma/migrations/20260318004611_init/migration.sql
@@ -0,0 +1,21 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "username" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Post" (
+    "id" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "authorId" TEXT NOT NULL,
+
+    CONSTRAINT "Post_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "Post" ADD CONSTRAINT "Post_authorId_fkey" FOREIGN KEY ("authorId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
---
+++ src/users/entities/user.entity.ts
@@ -0,0 +1,8 @@
+export class User {
+    /* A REVISAR
+    id : string;
+    username : string;
+    password : string;
+    posts : string[];
+    */
+}
